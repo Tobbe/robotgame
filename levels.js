22279e97a7ab0a2d0de0c3a09f4f3759a62b1c64
Tobbe Lundberg <tobbe@tlundberg.com>
2016-01-11 22:54:18 +0100

    Make it easier to create new levels
    More can be kept as-is when copy/pasteing an old level in order
    to create a new one.
    
    Fixes #17

--- levels.js
+++ levels.js
@@ -13,26 +13,19 @@
  * E = Green Door
  * F = Blue Door
  */
-var levels = [{}, {}, {}, {}, {}, {}];
-levels[0].name = "1: Intro";
-levels[0].field = [
-    ['6-', 'E-', 'E-', 'E-', 'E-', 'C-'],
-    ['7-', 'F-', 'F-', 'F-', 'F-', 'D-'],
-    ['7-', 'FB', 'FA', 'FB', 'F-', 'D-'],
-    ['7-', 'F-', 'F-', 'F-', 'F-', 'D-'],
-    ['7-', 'F-', 'F-', 'F-', 'F-', 'D-'],
-    ['3-', 'B-', 'B-', 'B-', 'B-', '9-']];
-levels[0].items = [[], [], [], [], [], []];
-levels[0].items[2][1] = {
-        key: 'buttons',
-        index: '0'
-    };
-levels[0].items[2][3] = {
-        key: 'buttons',
-        index: '1'
-    };
-levels[0].leds = [{on: false}, {on: false}];
-levels[0].buttons = [{
+var levels = [];
+levels.push({
+    name: "1: Intro",
+    field:[
+        ['6-', 'E-', 'E-', 'E-', 'E-', 'C-'],
+        ['7-', 'F-', 'F-', 'F-', 'F-', 'D-'],
+        ['7-', 'FB', 'FA', 'FB', 'F-', 'D-'],
+        ['7-', 'F-', 'F-', 'F-', 'F-', 'D-'],
+        ['7-', 'F-', 'F-', 'F-', 'F-', 'D-'],
+        ['3-', 'B-', 'B-', 'B-', 'B-', '9-']],
+    items: [[], [], [], [], [], []],
+    leds: [{on: false}, {on: false}],
+    buttons: [{
         controlls: {
             key: 'leds',
             index: 0
@@ -42,64 +35,52 @@ levels[0].buttons = [{
             key: 'leds',
             index: 1
         }
-    }];
+    }]
+});
+levels[levels.length - 1].items[2][1] = {
+    key: 'buttons',
+    index: '0'
+};
+levels[levels.length - 1].items[2][3] = {
+    key: 'buttons',
+    index: '1'
+};
 
-levels[1].name = "2: Maze";
-levels[1].field = [
-    ['4A', '0-', '0-', '0-', '0-', '0-'],
-    ['5-', '0-', '0-', '6-', 'C-', '0-'],
-    ['3-', 'A-', 'A-', '9-', '1B', '0-'],
-    ['0-', '0-', '0-', '0-', '0-', '0-'],
-    ['0-', '0-', '0-', '0-', '0-', '0-'],
-    ['0-', '0-', '0-', '0-', '0-', '0-']];
-levels[1].items = [[], [], [], [], [], []];
-levels[1].items[2][4] = {
-        key: 'buttons',
-        index: '0'
-    };
-levels[1].leds = [{on: false}];
-levels[1].buttons = [{
+levels.push({
+    name: "2: Maze",
+    field: [
+        ['4A', '0-', '0-', '0-', '0-', '0-'],
+        ['5-', '0-', '0-', '6-', 'C-', '0-'],
+        ['3-', 'A-', 'A-', '9-', '1B', '0-'],
+        ['0-', '0-', '0-', '0-', '0-', '0-'],
+        ['0-', '0-', '0-', '0-', '0-', '0-'],
+        ['0-', '0-', '0-', '0-', '0-', '0-']],
+    items: [[], [], [], [], [], []],
+    leds: [{on: false}],
+    buttons: [{
         controlls: {
             key: 'leds',
             index: 0
         }
-    }];
+    }]
+});
+levels[levels.length - 1].items[2][4] = {
+    key: 'buttons',
+    index: '0'
+};
 
-levels[2].name = "3: Doors";
-levels[2].field = [
-    ['4-', '0-', '6-', 'AD', 'C-', '0-'],
-    ['3B', 'AC', 'F-', 'AE', 'F-', 'C-'],
-    ['0-', '0-', '3-', 'AF', '9-', '5-'],
-    ['0-', '0-', '0-', '0-', '0-', '5-'],
-    ['0-', '0-', '0-', '0-', '0-', '5-'],
-    ['0-', '0-', '0-', '0-', '0-', '1B']];
-levels[2].items = [[], [], [], [], [], []];
-levels[2].items[1][0] = {
-        key: 'buttons',
-        index: '0'
-    };
-levels[2].items[5][5] = {
-        key: 'buttons',
-        index: '1'
-    };
-levels[2].items[0][3] = {
-        key: 'doors',
-        index: 0
-    };
-levels[2].items[1][3] = {
-        key: 'doors',
-        index: 1
-    };
-levels[2].items[2][3] = {
-        key: 'doors',
-        index: 2
-    };
-levels[2].items[1][1] = {
-        key: 'chests',
-        index: 0
-    };
-levels[2].leds = [{on: false}, {on: false}];
-levels[2].buttons = [{
+levels.push({
+    name: "3: Doors",
+    field: [
+        ['4-', '0-', '6-', 'AD', 'C-', '0-'],
+        ['3B', 'AC', 'F-', 'AE', 'F-', 'C-'],
+        ['0-', '0-', '3-', 'AF', '9-', '5-'],
+        ['0-', '0-', '0-', '0-', '0-', '5-'],
+        ['0-', '0-', '0-', '0-', '0-', '5-'],
+        ['0-', '0-', '0-', '0-', '0-', '1B']],
+    items: [[], [], [], [], [], []],
+    leds: [{on: false}, {on: false}],
+    buttons: [{
         controlls: {
             key: 'leds',
             index: 0
@@ -109,72 +90,104 @@ levels[2].buttons = [{
             key: 'leds',
             index: 1
         }
-    }];
-levels[2].doors = [{open: false}, {open: false}, {open: false}];
-levels[2].chests = [{open: false}];
+    }],
+    doors: [{open: false}, {open: false}, {open: false}],
+    chests: [{open: false}]
+});
+levels[levels.length - 1].items[1][0] = {
+    key: 'buttons',
+    index: '0'
+};
+levels[levels.length - 1].items[5][5] = {
+    key: 'buttons',
+    index: '1'
+};
+levels[levels.length - 1].items[0][3] = {
+    key: 'doors',
+    index: 0
+};
+levels[levels.length - 1].items[1][3] = {
+    key: 'doors',
+    index: 1
+};
+levels[levels.length - 1].items[2][3] = {
+    key: 'doors',
+    index: 2
+};
+levels[levels.length - 1].items[1][1] = {
+    key: 'chests',
+    index: 0
+};
 
-levels[3].name = "4: Loop";
-levels[3].field = [
-    ['2B', 'C-', '0-', '0-', '0-', '0-'],
-    ['0-', '3-', 'C-', '0-', '0-', '0-'],
-    ['0-', '0-', '3-', 'C-', '0-', '0-'],
-    ['0-', '0-', '0-', '3-', 'C-', '0-'],
-    ['0-', '0-', '0-', '0-', '3-', 'C-'],
-    ['0-', '0-', '0-', '0-', '0-', '1A']];
-levels[3].items = [[], [], [], [], [], []];
-levels[3].items[0][0] = {
-        key: 'buttons',
-        index: '0'
-    };
-levels[3].leds = [{on: false}];
-levels[3].buttons = [{
+levels.push({
+    name: "4: Loop",
+    field: [
+        ['2B', 'C-', '0-', '0-', '0-', '0-'],
+        ['0-', '3-', 'C-', '0-', '0-', '0-'],
+        ['0-', '0-', '3-', 'C-', '0-', '0-'],
+        ['0-', '0-', '0-', '3-', 'C-', '0-'],
+        ['0-', '0-', '0-', '0-', '3-', 'C-'],
+        ['0-', '0-', '0-', '0-', '0-', '1A']],
+    items: [[], [], [], [], [], []],
+    leds: [{on: false}],
+    buttons: [{
         controlls: {
             key: 'leds',
             index: 0
         }
-    }];
+    }]
+});
+levels[levels.length - 1].items[0][0] = {
+    key: 'buttons',
+    index: '0'
+};
 
-levels[4].name = "5: Six instructions";
-levels[4].field = [
-    ['0-', '6-', 'A-', 'A-', 'A-', 'CB'],
-    ['0-', '5-', '0-', '0-', '4-', '5-'],
-    ['0-', '3-', 'C-', '6-', 'B-', 'D-'],
-    ['0-', '0-', '7-', '9-', '0-', '5-'],
-    ['6-', '8A', '5-', '6-', 'C-', '5-'],
-    ['3-', 'A-', 'B-', '9-', '3-', '9-']];
-levels[4].items = [[], [], [], [], [], []];
-levels[4].items[0][5] = {
-        key: 'buttons',
-        index: '0'
-    };
-levels[4].leds = [{on: false}];
-levels[4].buttons = [{
+levels.push({
+    name: "5: Six instructions",
+    field: [
+        ['0-', '6-', 'A-', 'A-', 'A-', 'CB'],
+        ['0-', '5-', '0-', '0-', '4-', '5-'],
+        ['0-', '3-', 'C-', '6-', 'B-', 'D-'],
+        ['0-', '0-', '7-', '9-', '0-', '5-'],
+        ['6-', '8A', '5-', '6-', 'C-', '5-'],
+        ['3-', 'A-', 'B-', '9-', '3-', '9-']],
+    items: [[], [], [], [], [], []],
+    leds: [{on: false}],
+    buttons: [{
         controlls: {
             key: 'leds',
             index: 0
         }
-    }];
+    }]
+});
+levels[levels.length - 1].items[0][5] = {
+    key: 'buttons',
+    index: '0'
+};
 
-levels[5].name = "6: Conditional loop";
-levels[5].field = [
-    ['4A', '0-', '0-', '0-', '0-', '0-'],
-    ['3-', 'C-', '6-', '8-', '0-', '0-'],
-    ['0-', '3-', 'D-', '0-', '6-', '8B'],
-    ['0-', '0-', '3-', 'C-', '5-', '0-'],
-    ['0-', '0-', '0-', '3-', '9-', '0-'],
-    ['0-', '0-', '0-', '0-', '0-', '0-']];
-levels[5].items = [[], [], [], [], [], []];
-levels[5].items[2][5] = {
-        key: 'buttons',
-        index: '0'
-    };
-levels[5].leds = [{on: false}];
-levels[5].buttons = [{
+levels.push({
+    name: "6: Conditional loop",
+    field: [
+        ['4A', '0-', '0-', '0-', '0-', '0-'],
+        ['3-', 'C-', '6-', '8-', '0-', '0-'],
+        ['0-', '3-', 'D-', '0-', '6-', '8B'],
+        ['0-', '0-', '3-', 'C-', '5-', '0-'],
+        ['0-', '0-', '0-', '3-', '9-', '0-'],
+        ['0-', '0-', '0-', '0-', '0-', '0-']],
+    items: [[], [], [], [], [], []],
+    leds: [{on: false}],
+    buttons: [{
         controlls: {
             key: 'leds',
             index: 0
         }
-    }];
+    }]
+});
+levels[levels.length - 1].items[2][5] = {
+    key: 'buttons',
+    index: '0'
+};
+
 var currentLevel = -1;
 
 function getCurrentLevel() {
